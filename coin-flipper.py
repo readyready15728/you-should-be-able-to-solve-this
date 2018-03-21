5fb4998a7cb24ee66d4db03c6108496e11646d22
Lynn Bradshaw <readyready15728@gmail.com>
2018-03-21 03:51:51 -0400

    Upgrading project to Python 3

--- coin-flipper.py
+++ coin-flipper.py
@@ -1,10 +1,9 @@
-from __future__ import division
 import random
 
 total_with_at_least_one_head = 0
 total_with_two_heads = 0
 
-for _ in xrange(100000):
+for _ in range(100000):
     first_coin = random.choice([0, 1])
     second_coin = random.choice([0, 1])
 
@@ -16,4 +15,4 @@ for _ in xrange(100000):
         if first_coin == 1 and second_coin == 1:
             total_with_two_heads += 1
 
-print total_with_two_heads / total_with_at_least_one_head
+print(total_with_two_heads / total_with_at_least_one_head)
